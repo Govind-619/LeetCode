932624639f1c851db78969d611286557170a2c08
Govind S Kumar <govindskumar619@gmail.com>
2025-09-19 14:57:03 +0530

    Time: 1 ms (20.00%) | Memory: 4.2 MB (11.43%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.go
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.go
@@ -1,14 +1,10 @@
 func maxProduct(n int) int {
-    max1, max2 := 0, 0
-	for n > 0 {
-		d := n % 10
-		if d >= max1 {
-			max2 = max1
-			max1 = d
-		} else if d > max2 {
-			max2 = d
-		}
-		n /= 10
-	}
-	return max1 * max2
+    res:= []int{}
+    for n>0{
+        r:= n%10
+        res= append(res,r)
+        n /= 10
+    }
+    sort.Ints(res)
+    return res[len(res)-1]*res[len(res)-2]
 }
