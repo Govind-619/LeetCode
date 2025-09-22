7794f54a26dd38108176b47a7caf4bdd8bd15d56
Govind S Kumar <govindskumar619@gmail.com>
2025-09-22 15:42:29 +0530

    Time: 0 ms (100.00%) | Memory: 4.3 MB (83.72%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.go
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.go
@@ -2,18 +2,17 @@ func maxFrequencyElements(nums []int) int {
     m:= make(map[int]int)
 
     max:= 0
+    count:=0
     for _,v:= range nums{
         m[v]++
         if m[v]>max{
             max = m[v]
-        }
-    }
-    count:=0
-    for _, val:= range m{
-        if val == max{
-            count += val
+            count=1
+        }else if m[v]== max{
+            count++
         }
     }
     
-    return count
+   
+    return count* max
 }
