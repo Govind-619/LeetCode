cf33ee70eb7df14030cbe731a4135ca6b89829d6
Govind S Kumar <govindskumar619@gmail.com>
2025-09-29 19:02:54 +0530

    Time: 1 ms (54.55%) | Memory: 4.3 MB (36.36%) - LeetSync

---
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.go
@@ -0,0 +1,23 @@
+import "fmt"
+
+func minScoreTriangulation(values []int) int {
+    n := len(values)
+    dp := make([][]int, n)
+    for i := range dp {
+        dp[i] = make([]int, n)
+    }
+
+    for length := 2; length < n; length++ {
+        for i := 0; i+length < n; i++ {
+            j := i + length
+            dp[i][j] = 1<<31 - 1
+            for k := i + 1; k < j; k++ {
+                cost := dp[i][k] + dp[k][j] + values[i]*values[j]*values[k]
+                if cost < dp[i][j] {
+                    dp[i][j] = cost
+                }
+            }
+        }
+    }
+    return dp[0][n-1]
+}
