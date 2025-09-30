041e55dba4772c90d59ec448bdc44171c9b1a67f
Govind S Kumar <govindskumar619@gmail.com>
2025-09-30 11:29:32 +0530

    Time: 0 ms (100.00%) | Memory: 5.2 MB (92.26%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.go
+++ 4008-restore-finishing-order/restore-finishing-order.go
@@ -1,13 +1,15 @@
 func recoverOrder(order []int, friends []int) []int {
-    m:= make(map[int]bool)
-    for _,v:= range friends{
-        m[v]= true
-    }
-    res:= []int{}
-     for _,v:= range order{
-        if m[v]{
-            res= append(res,v)
+    var (
+        res = make([]int, len(friends))
+        i int
+    )
+
+    for _, v := range order {
+        if slices.Contains(friends, v) {
+            res[i] = v
+            i++
         }
-     }
+    }
+
     return res
 }
