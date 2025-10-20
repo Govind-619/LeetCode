2a687c4977e36a8e1a8f92e24be3bd3e6f833bc9
Govind S Kumar <govindskumar619@gmail.com>
2025-10-20 16:52:03 +0530

    Time: 0 ms (100.00%) | Memory: 5 MB (59.30%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.go
@@ -0,0 +1,11 @@
+func finalValueAfterOperations(operations []string) int {
+    x:=0
+    for _,v := range operations{
+        if strings.Contains(v,"+"){
+            x++
+        }else{
+            x--
+        }
+    }
+    return x
+}
