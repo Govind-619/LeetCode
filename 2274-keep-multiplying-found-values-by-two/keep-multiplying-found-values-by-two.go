7b87ae39f2db46b97d95d2aa7cc9dea592185da8
Govind S Kumar <govindskumar619@gmail.com>
2025-11-19 18:08:09 +0530

    Time: 0 ms (100.00%) | Memory: 5 MB (93.55%) - LeetSync

---
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.go
@@ -0,0 +1,8 @@
+func findFinalValue(nums []int, original int) int {
+  for _,v:= range nums{
+    if v == original{
+       return findFinalValue(nums,original*2)
+    }
+  } 
+  return original 
+}
