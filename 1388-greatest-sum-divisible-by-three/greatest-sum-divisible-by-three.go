8b42e47d3ea9180e80b948bad700da5659170efd
Govind S Kumar <govindskumar619@gmail.com>
2025-11-23 21:48:14 +0530

    Time: 9 ms (50.00%) | Memory: 8.7 MB (94.44%) - LeetSync

---
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.go
@@ -0,0 +1,21 @@
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+func maxSumDivThree(nums []int) int {
+    dp := [3]int{}
+    temp := [3]int{}
+    
+    for _, n := range nums {
+        temp = dp
+        for _, sum := range temp {
+            key := (sum+n) % 3 // reminder as key, key is index based
+            dp[key] = max(sum+n, dp[key])
+        }
+    }
+    
+    return dp[0]
+}
