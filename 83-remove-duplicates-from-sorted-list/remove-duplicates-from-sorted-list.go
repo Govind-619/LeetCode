09090d9f35d5f0a0041ea60fabe8707a480c6363
Govind S Kumar <govindskumar619@gmail.com>
2025-12-09 23:32:31 +0530

    Time: 0 ms (100.00%) | Memory: 4.9 MB (71.99%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+    if head == nil{
+        return head
+    }
+    current:= head 
+    for current.Next!=nil{
+        if current.Next.Val==current.Val{
+            current.Next= current.Next.Next
+            
+        }else{
+            current= current.Next
+        }
+    }
+    return head
+}
