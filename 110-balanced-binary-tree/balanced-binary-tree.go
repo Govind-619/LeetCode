21e1ae3b656983794d3f92d7c6dff27b348882a9
Govind S Kumar <govindskumar619@gmail.com>
2026-02-08 21:28:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (57.82%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.go
@@ -0,0 +1,24 @@
+func isBalanced(root *TreeNode) bool {
+    if root == nil{
+        return true
+    }
+    dif := maxDepth(root.Left) - maxDepth(root.Right)
+    if dif <= 1 && dif >= -1{
+        return isBalanced(root.Left) && isBalanced(root.Right)
+    }
+    return false
+}
+
+func maxDepth(root *TreeNode) int{
+    if root == nil{
+        return 0
+    }
+    return max(maxDepth(root.Left),maxDepth(root.Right))+1
+}
+
+func max(x int, y int) int{
+    if x > y{
+        return x
+    }
+    return y
+}
