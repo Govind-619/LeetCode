9e551af190ed7ba107671b6d8b951968ba51d26a
Govind S Kumar <govindskumar619@gmail.com>
2026-02-18 16:43:10 +0530

    Time: 0 ms (100.00%) | Memory: 3.8 MB (94.12%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.go
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.go
@@ -1,12 +1,4 @@
 func hasAlternatingBits(n int) bool {
-    c:= fmt.Sprintf("%b",n)
-    fmt.Println(c)
-    z:=rune(c[0])
-    for _,v:= range c[1:]{
-        if v==z{
-            return false
-        }
-        z=v
-    }
-    return true
+    x := n ^ (n >> 1)
+    return (x & (x + 1)) == 0
 }
