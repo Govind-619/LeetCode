06d1d1193eb08e6ec0aca014cfbb934c7169fff1
Govind S Kumar <govindskumar619@gmail.com>
2026-02-20 20:28:47 +0530

    Time: 0 ms (100.00%) | Memory: 4.2 MB (11.76%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.go
@@ -0,0 +1,32 @@
+import "sort"
+
+func makeLargestSpecial(s string) string {
+    var parts []string
+    count := 0
+    start := 0
+    
+    for i := 0; i < len(s); i++ {
+        if s[i] == '1' {
+            count++
+        } else {
+            count--
+        }
+        
+        if count == 0 {
+            inner := makeLargestSpecial(s[start+1 : i])
+            parts = append(parts, "1"+inner+"0")
+            start = i + 1
+        }
+    }
+    
+    sort.Slice(parts, func(i, j int) bool {
+        return parts[i] > parts[j]
+    })
+    
+    result := ""
+    for _, p := range parts {
+        result += p
+    }
+    
+    return result
+}
