2316b0a4e0d240e08613f8860ebe3cbb1419b534
Govind S Kumar <govindskumar619@gmail.com>
2026-02-21 20:18:16 +0530

    Time: 0 ms (100.00%) | Memory: 3.9 MB (56.45%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.go
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.go
@@ -1,23 +1,11 @@
-var primeSet = map[int]bool{
-    2:true, 3:true, 5:true, 7:true, 11:true, 13:true,
-    17:true, 19:true, 23:true, 29:true, 31:true,
-}
 
-func countPrimeSetBits(left int, right int) int {
-    count := 0
-    for i := left; i <= right; i++ {
-        if primeSet[countOnes(i)] {
-            count++
-        }
-    }
-    return count
-}
+func countPrimeSetBits(left int, right int) (cnt int) {
+	for i := uint(left); i <= uint(right); i++ {
+		switch bits.OnesCount(i) {
+		case 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31:
+			cnt++
+		}
+	}
 
-func countOnes(x int) int {
-    count := 0
-    for x > 0 {
-        x = x & (x - 1)
-        count++
-    }
-    return count
+	return
 }
