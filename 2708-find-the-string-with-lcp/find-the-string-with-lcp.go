fb296960d3823e5864e3ec6294d52249aa7e81f3
Govind S Kumar <govindskumar619@gmail.com>
2026-03-28 22:42:32 +0530

    Time: 8 ms (25.00%) | Memory: 28.8 MB (25.00%) - LeetSync

---
+++ 2708-find-the-string-with-lcp/find-the-string-with-lcp.go
@@ -0,0 +1,55 @@
+func findTheString(lcp [][]int) string {
+	n := len(lcp)
+
+	group := make([]int, n)
+	for i := 0; i < n; i++ {
+		group[i] = -1
+	}
+
+	curGroup := 0
+
+	for i := 0; i < n; i++ {
+		if group[i] == -1 {
+			if curGroup == 26 {
+				return ""
+			}
+
+			group[i] = curGroup
+			curGroup++
+
+			for j := i + 1; j < n; j++ {
+				if lcp[i][j] > 0 {
+					group[j] = group[i]
+				}
+			}
+		}
+	}
+
+	ans := make([]byte, n)
+	for i := 0; i < n; i++ {
+		ans[i] = byte('a' + group[i])
+	}
+
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		for j := n - 1; j >= 0; j-- {
+			if ans[i] == ans[j] {
+				dp[i][j] = 1 + dp[i+1][j+1]
+			}
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if dp[i][j] != lcp[i][j] {
+				return ""
+			}
+		}
+	}
+
+	return string(ans)
+}
