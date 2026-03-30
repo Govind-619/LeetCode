33fef8eb2516bb196325784de6c01c422057c81b
Govind S Kumar <govindskumar619@gmail.com>
2026-03-30 20:46:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (47.06%) - LeetSync

---
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.go
@@ -0,0 +1,22 @@
+func checkStrings(s1 string, s2 string) bool {
+    even := make([]int, 26)
+    odd := make([]int, 26)
+
+    for i := 0; i < len(s1); i++ {
+        if i%2 == 0 {
+            even[s1[i]-'a']++
+            even[s2[i]-'a']--
+        } else {
+            odd[s1[i]-'a']++
+            odd[s2[i]-'a']--
+        }
+    }
+
+    for i := 0; i < 26; i++ {
+        if even[i] != 0 || odd[i] != 0 {
+            return false
+        }
+    }
+
+    return true
+}
