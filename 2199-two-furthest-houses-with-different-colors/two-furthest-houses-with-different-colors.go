2774f0bf207bd3395391b0eda122c2dc4b32d4bf
Govind S Kumar <govindskumar619@gmail.com>
2026-04-21 21:29:40 +0530

    Time: 0 ms (100.00%) | Memory: 4.2 MB (29.41%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.go
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.go
@@ -1,21 +1,14 @@
 func maxDistance(colors []int) int {
-    n:= len(colors)
-    max:=0
-    for i:=0; i<n;i++{
-        for j:=n-1; j>i; j--{
-            if colors[i]!=colors[j]{
-                max= j-i
-            }
-        }
-    }
-    for j:=n-1; j>=0; j--{
-        for i:=0; i<j;i++{
-            if colors[i]!=colors[j]{
-                if j-i>max{
-                     max= j-i
-                }
-            }
-        }
-    }
-    return max
+	n := len(colors)
+	ans := 0
+
+	for i := 1; i < n; i++ {
+		if colors[i] != colors[0] {
+			ans = max(ans, i)
+		}
+		if colors[i] != colors[n-1] {
+			ans = max(ans, n-1-i)
+		}
+	}
+	return ans
 }
