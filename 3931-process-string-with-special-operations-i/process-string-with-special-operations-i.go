71685c3bb4ef22a362eac823f96d986d9d1aa122
Govind S Kumar <govindskumar619@gmail.com>
2026-06-16 12:31:35 +0530

    Time: 326 ms (6.67%) | Memory: 29.8 MB (6.67%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.go
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.go
@@ -1,29 +1,62 @@
-import "unicode"
+package main
+
+import (
+	"strings"
+)
 
 func processStr(s string) string {
-    result:=[]rune{}
-    for _,v:= range s{
-        if unicode.IsLetter(v){
-            result= append(result,v)
-        }else if v=='#'{
-            result= append(result,result...)
-        }else if v=='%'{
-            reverse(result)
-        }else{
-            l:= len(result)
-            if l<2{
-                result =[]rune{}
-            }else{
-                result= result[:l-1]
-            }
-        }
-    }
-    return string(result)
-}
+	// chunks stores the valid string segments processed so far
+	chunks := make([]string, 0, len(s))
 
-func reverse(s []rune){
-    n:= len(s)
-    for i,j:=0,n-1; i<j; i,j = i+1, j-1{
-        s[i], s[j]= s[j], s[i]
-    }
+	for i := 0; i < len(s); i++ {
+		ch := s[i]
+
+		switch ch {
+		case '*': // Backspace operation
+			// Find the last non-empty chunk and remove its last character
+			for len(chunks) > 0 {
+				lastIdx := len(chunks) - 1
+				if len(chunks[lastIdx]) > 0 {
+					chunks[lastIdx] = chunks[lastIdx][:len(chunks[lastIdx])-1]
+					// If the chunk becomes empty, pop it entirely
+					if len(chunks[lastIdx]) == 0 {
+						chunks = chunks[:lastIdx]
+					}
+					break
+				} else {
+					chunks = chunks[:lastIdx]
+				}
+			}
+
+		case '%': // Reverse operation
+			if len(chunks) == 0 {
+				continue
+			}
+			// 1. Reverse the order of the chunks themselves
+			for left, right := 0, len(chunks)-1; left < right; left, right = left+1, right-1 {
+				chunks[left], chunks[right] = chunks[right], chunks[left]
+			}
+			// 2. Reverse the internal characters of each individual chunk
+			for j := 0; j < len(chunks); j++ {
+				runes := []rune(chunks[j])
+				for left, right := 0, len(runes)-1; left < right; left, right = left+1, right-1 {
+					runes[left], runes[right] = runes[right], runes[left]
+				}
+				chunks[j] = string(runes)
+			}
+
+		case '#': // Duplicate operation
+			if len(chunks) == 0 {
+				continue
+			}
+			// Safely duplicate the current state by appending the existing chunks to themselves
+			chunks = append(chunks, chunks...)
+
+		default: // Normal lowercase letters
+			chunks = append(chunks, string(ch))
+		}
+	}
+
+	// Join all processed chunks into the final string
+	return strings.Join(chunks, "")
 }
