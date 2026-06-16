3bd95ed8b60ffacd31513ce2ee66188512f0116d
Govind S Kumar <govindskumar619@gmail.com>
2026-06-16 12:38:23 +0530

    Time: 4 ms (56.67%) | Memory: 11 MB (43.33%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.go
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.go
@@ -1,62 +1,34 @@
 package main
 
-import (
-	"strings"
-)
-
 func processStr(s string) string {
-	// chunks stores the valid string segments processed so far
-	chunks := make([]string, 0, len(s))
+	// Pre-allocate a reasonable capacity to avoid early re-allocations
+	res := make([]byte, 0, len(s)*2)
 
 	for i := 0; i < len(s); i++ {
 		ch := s[i]
 
 		switch ch {
-		case '*': // Backspace operation
-			// Find the last non-empty chunk and remove its last character
-			for len(chunks) > 0 {
-				lastIdx := len(chunks) - 1
-				if len(chunks[lastIdx]) > 0 {
-					chunks[lastIdx] = chunks[lastIdx][:len(chunks[lastIdx])-1]
-					// If the chunk becomes empty, pop it entirely
-					if len(chunks[lastIdx]) == 0 {
-						chunks = chunks[:lastIdx]
-					}
-					break
-				} else {
-					chunks = chunks[:lastIdx]
-				}
+		case '*':
+			if len(res) > 0 {
+				res = res[:len(res)-1] // O(1) pop
 			}
-
-		case '%': // Reverse operation
-			if len(chunks) == 0 {
-				continue
+			
+		case '#':
+			// Go's built-in append uses highly optimized memory copying under the hood.
+			// This safely handles exponential growth without any bounds panics.
+			res = append(res, res...)
+			
+		case '%':
+			// Physical reverse in a flat slice.
+			// Because memory is contiguous, the CPU cache absolutely crushes this loop.
+			for l, r := 0, len(res)-1; l < r; l, r = l+1, r-1 {
+				res[l], res[r] = res[r], res[l]
 			}
-			// 1. Reverse the order of the chunks themselves
-			for left, right := 0, len(chunks)-1; left < right; left, right = left+1, right-1 {
-				chunks[left], chunks[right] = chunks[right], chunks[left]
-			}
-			// 2. Reverse the internal characters of each individual chunk
-			for j := 0; j < len(chunks); j++ {
-				runes := []rune(chunks[j])
-				for left, right := 0, len(runes)-1; left < right; left, right = left+1, right-1 {
-					runes[left], runes[right] = runes[right], runes[left]
-				}
-				chunks[j] = string(runes)
-			}
-
-		case '#': // Duplicate operation
-			if len(chunks) == 0 {
-				continue
-			}
-			// Safely duplicate the current state by appending the existing chunks to themselves
-			chunks = append(chunks, chunks...)
-
-		default: // Normal lowercase letters
-			chunks = append(chunks, string(ch))
+			
+		default:
+			res = append(res, ch) // Amortized O(1) append
 		}
 	}
 
-	// Join all processed chunks into the final string
-	return strings.Join(chunks, "")
+	return string(res)
 }
