a255b1f44d2e2396e4bee460c7f86da7ba880fa2
Govind S Kumar <govindskumar619@gmail.com>
2026-06-26 20:03:50 +0530

    Time: 43 ms (5.56%) | Memory: 11.2 MB (83.33%) - LeetSync

---
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.go
@@ -0,0 +1,56 @@
+func countMajoritySubarrays(nums []int, target int) int64 {
+
+	n := len(nums)
+
+	pref := make([]int, n+1)
+	for i := 0; i < n; i++ {
+		if nums[i] == target {
+			pref[i+1] = pref[i] + 1
+		} else {
+			pref[i+1] = pref[i] - 1
+		}
+	}
+
+	values := append([]int{}, pref...)
+	sort.Ints(values)
+
+	k := 0
+	for _, x := range values {
+		if k == 0 || values[k-1] != x {
+			values[k] = x
+			k++
+		}
+	}
+	values = values[:k]
+
+	bit := make([]int, k+2)
+
+	update := func(idx int) {
+		for idx < len(bit) {
+			bit[idx]++
+			idx += idx & -idx
+		}
+	}
+
+	query := func(idx int) int64 {
+		var sum int64
+		for idx > 0 {
+			sum += int64(bit[idx])
+			idx -= idx & -idx
+		}
+		return sum
+	}
+
+	var ans int64
+
+	for _, x := range pref {
+
+		idx := sort.SearchInts(values, x) + 1
+
+		ans += query(idx - 1)
+
+		update(idx)
+	}
+
+	return ans
+}
