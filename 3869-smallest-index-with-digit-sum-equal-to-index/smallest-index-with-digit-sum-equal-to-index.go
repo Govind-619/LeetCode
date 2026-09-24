448a89d72c73fc8457f9ede8cfb50d953c5bce4d
Govind S Kumar <govindskumar619@gmail.com>
2026-09-24 21:25:22 +0530

    Time: 0 ms (100.00%) | Memory: 4.9 MB (84.00%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.go
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.go
@@ -1,9 +1,10 @@
 func smallestIndex(nums []int) int {
+    
     for i,v:= range nums{
         sum:=0
         for v>0{
             sum += v%10
-            v /= 10
+            v /=10
         }
         if sum == i{
             return i
